bfa85b67b4209e6a5985357ff893f0cb76c01aa8
George Ciesinski <george.ciesin@gmail.com>
2022-11-10 23:26:13 -0500

    Added basic styling for page - Header - Main - Footer - Button - Modal - Book Shelf

--- js/library.js
+++ js/library.js
@@ -1,3 +1,26 @@
+// Modal
+const modal = document.querySelector("#book-modal");
+const addBook = document.querySelector("#add-book");
+const close = document.querySelector("#close");
+
+// Show modal
+addBook.addEventListener("click", function() {
+    console.log("working");
+    modal.style.display = "block";
+});
+
+// Hide modal when close clicked
+close.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+// Hide modal when outside modal clicked
+window.addEventListener("click", function() {
+    if (this.event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
 // Update copyright message in dom with current year
 const updateCopyright = function() {
     const copyrightMessage = document.querySelector("#copyright-message");
